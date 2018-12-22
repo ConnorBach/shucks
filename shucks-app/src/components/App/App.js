f5a15876c63d45b0fee9fb94322e71f1b8d128b9
Michael Orwin <michaelxorwin@gmail.com>
2018-12-21 21:19:41 -0500

    show usernames

--- shucks-app/src/components/App/App.js
+++ shucks-app/src/components/App/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import MessageBoard from '../MessageBoard/MessageBoard';
+import MessageMaker from '../MessageMaker/MessageMaker';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div className="App">
---
+++ shucks-app/src/components/Message/Message.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './Message.css';
+
+class Message extends Component {
+  constructor(props) {
+    super(props);
+    this.username = props.username
+  }
+  render() {
+    return (
+      <div className="Message">
+        <div className={this.props.username == "orwinmc" ? "sent" : "recieved"}>
+          <div className="messageBubble">
+            {this.props.msg}
+          </div>
+          <br clear="all" />
+          <div className="userInfo">
+            {this.props.username}
+          </div>
+          <br clear="all" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Message;
